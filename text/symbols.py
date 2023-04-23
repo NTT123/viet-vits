dfc4eea70045d61c22ff73f0859fff79ec5b1bc9
ntt123 <xcodevn@gmail.com>
2023-04-23 23:52:34 +0800

    add infore dataset

--- text/symbols.py
+++ text/symbols.py
@@ -5,12 +5,43 @@ Defines the set of symbols used in text input to the model.
 """
 _pad = "_"
 _punctuation = ';:,.!?¡¿—…"«»“” '
-_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-_letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
+_letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "g",
+    "h",
+    "i",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "x",
+    "y",
+    "đ",
+    "̀",
+    "́",
+    "̂",
+    "̃",
+    "̆",
+    "̉",
+    "̛",
+    "̣",
+]
 
 
 # Export all symbols:
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+symbols = [_pad] + list(_punctuation) + _letters
 
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
